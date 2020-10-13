82e546ffb5d67b629203af8e92e2e8f9244be3fa
Ana Saunier <anasaunier@air-de-ana.lan>
2020-10-13 11:49:00 -0300

    cambios

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-window.onload = function() {
 
+alert ("Bienvenidos a mi sitio");
+let confirmacion = confirm ("Esta seguro de querer avanzar?");
+titulo = document.querySelector(".titulo");
+if (confirmacion == false) {titulo.innerHTML ="Lamentamos que no quieras continuar tu visita por este sitio increíble"}
+  else if (confirmacion == true) {titulo.innerHTML ="¡Qué alegría que quieras seguir con tu visita por este maravilloso sitio!" 
+  nombre = prompt ("ingrese su nombre"); }
 
+  saludo = document.querySelector(".saludar");
+  saludo.innerHTML = "Bienvenido " + nombre;
 
-    alert("bienvenidos a mi sitio");
-let confirmacion  = confirm("esta seguro de querer avanzar?");
-let titulo  = document.querySelector(".titulo");
-if (confirmacion == false)
-{titulo.innerHTML = "Lamentamos que no quieras continuar tu visita por este sitio increíble."}
+  edad = prompt ("ingrese su edad");
+  if (edad <18) {acceso = alert("prohibido su acceso")}
+  else if (edad>=18){acceso = alert("permitido su acceso"); 
 
-else if (confirmacion = true) {titulo.innerHTML = "¡Qué alegría que quieras seguir con tu visita por este maravilloso sitio!" }
-}
-
-let  nombre = prompt("ingresa tu nombre");
-let  saludo = document.querySelector("h1");
-saludo.innerHTML = "hola " + nombre
-
-let edad = prompt("ingresa tu edad")
-if (edad >= 18) {
-
-} else {
-
-}
-
-let modooscuro = confirm("deseas cambiar a modo oscuro?")
+let modooscuro = confirm("deseas cambiar a modo oscuro?" )
 let body = document.querySelector("body")
+ 
+if (modooscuro == true) {
+ body.style.backgroundColor = "dimgray"
+ body.style.color = "white" }
 
-if (modooscuro == true){
-    body.style.backgroundColor = "dimgray"
-    body.style.color = "white"
-}
+  let diaDeSemana = "Domingo"
+ if (diaDeSemana == "Domingo") {
+    console.log("¡Hoy es día de descanso!") }
+ 
 
-let diaDeSemana = "Domingo"
-if (diaDeSemana == "Domingo") {
-    console.log("¡Hoy es día de descanso!")
-}
 
+  
+  
+  
+  
+  
+ 
+  
+ 
 
 
